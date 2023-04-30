4016f635df8cba384ef6300723840d97729291c4
idd_umoreira <idd_umoreira@uolinc.com>
2023-04-30 11:45:38 -0300

    criando funcionalidade para lancamentos

--- src/main/java/com/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
@@ -3,6 +3,9 @@ package com.algamoney.api.repository.lancamento;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 
 import com.algamoney.api.model.Lancamento;
@@ -22,7 +25,7 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 	private EntityManager manager;
 
 	@Override
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable) {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Lancamento> criteriaQuery = builder.createQuery(Lancamento.class);
 		Root<Lancamento> root = criteriaQuery.from(Lancamento.class);
@@ -32,7 +35,9 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 		criteriaQuery.where(predicates);
 
 		TypedQuery<Lancamento> query = manager.createQuery(criteriaQuery);
-		return query.getResultList();
+		adicionarRestricoesDePaginacao(query, pageable);
+
+		return new PageImpl<Lancamento>(query.getResultList(), pageable, total(lancamentoFilter));
 
 	}
 
@@ -61,6 +66,28 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 		}
 
 		return predicates.toArray(new Predicate[predicates.size()]);
+	}
+
+	private void adicionarRestricoesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
+		int paginaAtual = pageable.getPageNumber();
+		int totalregistroPorPagina = pageable.getPageSize();
+		int primeiroregistroPagina = paginaAtual * totalregistroPorPagina;
+
+		query.setFirstResult(primeiroregistroPagina);
+		query.setMaxResults(totalregistroPorPagina);
+	}
+
+	private Long total(LancamentoFilter lancamentoFilter) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Long> criteriaQuery = builder.createQuery(Long.class);
+		Root<Lancamento> root = criteriaQuery.from(Lancamento.class);
+
+//		criar restrições
+		Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
+		criteriaQuery.where(predicates);
+
+		criteriaQuery.select(builder.count(root));
+		return manager.createQuery(criteriaQuery).getSingleResult();
 	}
 
 }
--- src/main/java/com/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/algamoney/api/resource/LancamentoResource.java
@@ -1,10 +1,11 @@
 package com.algamoney.api.resource;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -40,8 +41,8 @@ public class LancamentoResource {
 	private ApplicationEventPublisher publisher;
 
 	@GetMapping
-	public List<Lancamento> listarLancamento(LancamentoFilter lancamentoFilter) {
-		return lancamentoRepository.filtrar(lancamentoFilter);
+	public Page<Lancamento> listarLancamento(LancamentoFilter lancamentoFilter,Pageable pageable) {
+		return lancamentoRepository.filtrar(lancamentoFilter,pageable);
 	}
 
 	@GetMapping("/{codigo}")
