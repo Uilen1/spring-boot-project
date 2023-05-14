96add886b3e689d50dda1bd2f33d38e0e670f524
idd_umoreira <idd_umoreira@uolinc.com>
2023-05-14 19:02:08 -0300

    criando autenticação oauth2

--- src/main/java/com/algamoney/api/config/AuthSecurityConfig.java
+++ src/main/java/com/algamoney/api/config/AuthSecurityConfig.java
@@ -2,8 +2,13 @@ package com.algamoney.api.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.UUID;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +19,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -22,6 +30,12 @@ import org.springframework.security.oauth2.server.authorization.settings.Authori
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 @Configuration
 @EnableWebSecurity
@@ -48,15 +62,15 @@ public class AuthSecurityConfig {
     RegisteredClientRepository registeredClientRepository (PasswordEncoder passwordEncoder) {
     	
     	RegisteredClient userClient = RegisteredClient
-    			.withId("1")
-    			.clientId("admin")
-    			.clientSecret(passwordEncoder.encode("admin"))
+    			.withId("0")
+    			.clientId("user")
+    			.clientSecret(passwordEncoder.encode("user"))
     			.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
     			.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-    			.scope("read")
-    			.scope("write")
+    			.scope("users:read")
+    			.scope("users:write")
     			.tokenSettings(TokenSettings.builder()
-    					.accessTokenTimeToLive(Duration.ofMinutes(2))
+    					.accessTokenTimeToLive(Duration.ofMinutes(20))
     					.build())
     			.clientSettings(ClientSettings.builder()
     					.requireAuthorizationConsent(false)
@@ -64,9 +78,72 @@ public class AuthSecurityConfig {
     			.build()
     			;
     	
-    	return new InMemoryRegisteredClientRepository(Arrays.asList(userClient));
+    	RegisteredClient blogClient = RegisteredClient
+    			.withId("1")
+    			.clientId("blog")
+    			.clientSecret(passwordEncoder.encode("blog"))
+    			.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+    			.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+    			.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+    			.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+    			.redirectUri("http://localhost:8080/lancamentos")
+    			.redirectUri("https://oidcdebugger.com/debug")
+    			.redirectUri("https://oauth.pstmn.io/v1/callback")
+    			.scope("myUser:read")
+    			.scope("myUser:write")
+    			.scope("posts:write")
+    			.tokenSettings(TokenSettings.builder()
+    					.accessTokenTimeToLive(Duration.ofMinutes(20))
+    					.refreshTokenTimeToLive(Duration.ofDays(1))
+    					.reuseRefreshTokens(false)
+    					.build())
+    			.clientSettings(ClientSettings.builder()
+    					.requireAuthorizationConsent(true)
+    					.build())
+    			.build()
+    			;
+    	
+    	return new InMemoryRegisteredClientRepository(Arrays.asList(userClient,blogClient));
     	
     }
+
+
+    @Bean
+    JWKSource<SecurityContext> jwkSource() {
+		KeyPair keyPair = generateRsaKey();
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		RSAKey rsaKey = new RSAKey.Builder(publicKey)
+				.privateKey(privateKey)
+				.keyID(UUID.randomUUID().toString())
+				.build();
+		JWKSet jwkSet = new JWKSet(rsaKey);
+		return new ImmutableJWKSet<>(jwkSet);
+	}
+
+	private static KeyPair generateRsaKey() { 
+		KeyPair keyPair;
+		try {
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			keyPair = keyPairGenerator.generateKeyPair();
+		}
+		catch (Exception ex) {
+			throw new IllegalStateException(ex);
+		}
+		return keyPair;
+	}
+
+    @Bean
+    JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+		return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+	}
+    
+    @Bean
+    JwtEncoder jwtEncoder (JWKSource<SecurityContext> jwtSource) {
+		return new NimbusJwtEncoder(jwtSource);
+    }
+
 
     @Bean
     AuthorizationServerSettings authorizationServerSettings() {
--- src/main/java/com/algamoney/api/config/SecurityConfig.java
+++ src/main/java/com/algamoney/api/config/SecurityConfig.java
@@ -5,14 +5,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
+/*
+ * Autenticação Básica
+ * 
+ * */
 @Configuration
 public class SecurityConfig {
 
@@ -33,15 +32,15 @@ public class SecurityConfig {
 	    return http.build();
 	  }
 
-    @Bean
-    UserDetailsService userDetailsService() {
-	    UserDetails user = User.builder()
-	    		.username("admin")
-	    		.password(passwordEncoder().encode("admin"))
-	    		.roles("ROLE")
-	    		.build();
-	    return new InMemoryUserDetailsManager(user);
-	  }
+//    @Bean
+//    UserDetailsService userDetailsService() {
+//	    UserDetails user = User.builder()
+//	    		.username("admin")
+//	    		.password(passwordEncoder().encode("admin"))
+//	    		.roles("ROLE")
+//	    		.build();
+//	    return new InMemoryUserDetailsManager(user);
+//	  }
 
     @Bean
     PasswordEncoder passwordEncoder() {
